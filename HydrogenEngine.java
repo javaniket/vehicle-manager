fa73a8fb025230d50d86fa499942f333959acdae
Aniket Garg <aniket@gmail.com>
2023-03-07 20:01:59 +0530

    Stop Hydrogen Engine in vehicle Class

--- HydrogenEngine.java
+++ HydrogenEngine.java
@@ -2,4 +2,7 @@ public class HydrogenEngine{
 	public void start() {
 		System.out.println("Hydrogen Engine Started");
 	}
+	public void stop() {
+		System.out.println("Hydrogen Engine Stopped");
+	}
 }
